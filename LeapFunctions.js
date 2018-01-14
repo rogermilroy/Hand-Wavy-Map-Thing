648d7c2a7fb2648f4b533789d26ee98d8b75fd22
Marcus Messer <marcus.messer.2016@live.rhul.ac.uk>
2018-01-14 07:08:03 +0000

    More shit for you to test.

--- LeapFunctions.js
+++ LeapFunctions.js
@@ -13,33 +13,48 @@ function LeapFunctions(type) {
 
     this.mapMovement = function mapMovement(frame) {
         if (frame.hands.length > 0) {
-            for (var i = 0; i < frame.hands.length; i++) {
-                var hand = frame.hands[i];
-                if (!this.screenGrabbed) {
-                    if (hand.grabStrength >= 0.9) {
-                        this.screenGrabbed = true;
-                        this.startVector = hand.palmPosition;
-                        this.direction = [hand.translation[0], hand.translation[2]];
-                        console.log("Screen Grabbed: " + this.screenGrabbed);
-                        console.log("Grab Start Vector: " + this.startVector);
-                        console.log("Direction" + this.direction);
+            if (hasTwoHands(frame)) {
+                if (bothClosed(frame.hands)) {
+                    if(handsOpposing(frame.hands, previousFrame)) {
+                        var closing = handsClosing(frame.hands, previousFrame);
+                        if (closing > 0) {
+                            console.log("Zoom in");
+                        } else if (closing < 0) {
+                            console.log("Zoom out");
+                        } else {
+                            console.log("No Zoom");
+                        }
                     }
                 }
+            } else {
+                for (var i = 0; i < frame.hands.length; i++) {
+                    var hand = frame.hands[i];
+                    if (!this.screenGrabbed) {
+                        if (hand.grabStrength >= 0.9) {
+                            this.screenGrabbed = true;
+                            this.startVector = hand.palmPosition;
+                            this.direction = [hand.translation[0], hand.translation[2]];
+                            console.log("Screen Grabbed: " + this.screenGrabbed);
+                            console.log("Grab Start Vector: " + this.startVector);
+                            console.log("Direction" + this.direction);
+                        }
+                    }
 
-                if (screenGrabbed) {
-                    if (hand.grabStrength === 0) {
-                        screenGrabbed = false;
-                        console.log("Screen Grabbed: " + screenGrabbed);
+                    if (screenGrabbed) {
+                        if (hand.grabStrength === 0) {
+                            screenGrabbed = false;
+                            console.log("Screen Grabbed: " + screenGrabbed);
+                        }
                     }
                 }
+                return screenGrabbed;
             }
-            return screenGrabbed;
         }
     };
 
     this.getMovementDirection = function () {
 
-    }
+    };
 
     /*this.menuGestures = function menuGestures(frame) {
         if (frame.gestures.length > 0) {
